8be49c63b39a77d14af130c945606408391d6f2d
Angad Singh <mail@angad.dev>
2020-06-25 02:02:53 +0530

    Remove email verification

--- backend/api/auth/register.js
+++ backend/api/auth/register.js
@@ -1,38 +1,38 @@
-const bcrypt = require('bcrypt')
-const nodemailer = require('nodemailer')
-const { against, generateVerificationURL } = require('../../lib/validation')
-const recaptcha = require('../../lib/recaptcha')
-const logs = require('../../lib/logs')
-const { client } = require('../../lib/prisma')
+const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
+const { against, generateVerificationURL } = require("../../lib/validation");
+const recaptcha = require("../../lib/recaptcha");
+const logs = require("../../lib/logs");
+const { client } = require("../../lib/prisma");
 
 async function sendMail(email, url) {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.zoho.in',
+    host: "smtp.zoho.in",
     port: 465,
     secure: true,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
     },
-  })
+  });
 
   // send mail with defined transport object
   const info = await transporter.sendMail({
     from: '"Team Cryptocracy" <admin@cryptichunt.com>',
     to: email, // list of receivers
-    subject: 'Please verify your email', // Subject line
+    subject: "Please verify your email", // Subject line
     html: `Click the following link - <a href="${url}">${url}</a>`, // html body
-  })
+  });
 
-  return info
+  return info;
 }
 
 module.exports = [
   recaptcha.verify(),
-  against(require('./register-schema')),
+  against(require("./register-schema")),
   async (req, res, next) => {
     try {
-      const { email, username, password, discord, edu, name } = req.body
+      const { email, username, password, discord, edu, name } = req.body;
 
       const user = await client.user.create({
         data: {
@@ -43,39 +43,39 @@ module.exports = [
           edu,
           name,
           points: 200,
-          emailVerified: false,
+          emailVerified: true,
           discordVerified: true,
           currentTile: { connect: { id: 1 } },
         },
-      })
+      });
 
       await client.visitedTile.create({
         data: {
           tile: { connect: { id: 1 } },
           user: { connect: { id: user.id } },
         },
-      })
+      });
 
-      const emailUrl = await generateVerificationURL('email', user)
-      await sendMail(user.email, emailUrl)
+      // const emailUrl = await generateVerificationURL('email', user)
+      // await sendMail(user.email, emailUrl)
 
-      await logs.add(user.id, `${user.username} accepted their mission`)
+      await logs.add(user.id, `${user.username} accepted their mission`);
 
       res.json({
         success: true,
-        message: 'User created',
+        message: "User created",
         id: user.id,
-      })
+      });
     } catch (e) {
       if (e.message.match(/Unique constraint/g)) {
         return res.json({
           success: false,
           message: `An account already exists with this ${e.meta.target}`,
-        })
+        });
       } else {
-        e.statusCode = 500
-        next(e)
+        e.statusCode = 500;
+        next(e);
       }
     }
   },
-]
+];
